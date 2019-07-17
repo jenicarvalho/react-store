b5d860d5e55b9205f1858137aec932264d8f7f26
Jeniffer Carvalho <jenicarvalho@live.com>
2019-07-16 21:19:51 -0300

    init

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+  MdDelete,
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+} from 'react-icons/md'
+import { Container, ProductTable, Total } from './styles'
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Qtd</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/slip-on-burn-still/06/E07-0352-006/E07-0352-006_detalhe1.jpg?resize=326:*"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis novo</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  )
+}
