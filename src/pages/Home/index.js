f73f9e39cc0db2f329dc2498131404ad1bf80cc5
Jeniffer Carvalho <jenicarvalho@live.com>
2019-07-17 10:43:55 -0300

    criado connect e adicionado dispatch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
-import { ProductList } from './styles'
+import { connect } from 'react-redux'
+
 import api from '../../services/api'
-
 import { formatPrice } from '../../util/format'
+import { ProductList } from './styles'
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   }
@@ -21,6 +22,14 @@ export default class Home extends Component {
     this.setState({ products: data })
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
   render() {
     const { products } = this.state
     return (
@@ -31,7 +40,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -43,3 +55,5 @@ export default class Home extends Component {
     )
   }
 }
+
+export default connect()(Home)
